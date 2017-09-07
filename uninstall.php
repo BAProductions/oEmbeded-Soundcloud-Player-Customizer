50f115c88915d5d3b7e1eb529f0b3a76711ba545
BAproductions <BAproductions@MacBook-Pro.local>
2017-09-07 10:13:53 -0400

    oEmbeded Soundcloud Player Customizer
    oEmbeded Soundcloud Player Customizer

--- uninstall.php
+++ uninstall.php
@@ -2,8 +2,23 @@
 if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
-$options = array( 'swap_player', 'show_artwork', 'play_button_color', 'auto_play', 'hide_related', 'show_comments', 'show_user', 'show_reposts' );
-foreach($options as $option_name){
-	unregister_setting('', $option_name, '');
-	delete_option($option_name);
+class oEmbedSoundcloudPlayerCustomizer_uninstaller
+{
+	public function __construct() {
+			// don't call add_shortcode here
+			// actually, I worked with wordpress last year and
+			// i think this is a good place to call add_shortcode 
+			// (and all other filters) now...
+	}
+	public function oEmbedSoundcloudPlayerCustomizer_uninstall(){
+		$options = array( 'swap_player', 'show_artwork', 'play_button_color', 'auto_play', 'hide_related', 'show_comments', 'show_user', 'show_reposts' );
+		foreach($options as $option_name){
+			unregister_setting('', $option_name, '');
+			delete_option($option_name);
+		}
+	}
+}
+if (class_exists('oEmbedSoundcloudPlayerCustomizer_uninstaller')){
+	$oEmbedSoundcloudPlayerCustomizer_uninstaller = new oEmbedSoundcloudPlayerCustomizer_uninstaller();
+	$oEmbedSoundcloudPlayerCustomizer_uninstaller->oEmbedSoundcloudPlayerCustomizer_uninstall();
 }
